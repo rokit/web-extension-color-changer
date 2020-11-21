db8790e6c772d7b9e8d006916175c7e563cad628
Luke Simpson <goodvibrato@gmail.com>
2020-11-21 16:58:40 -0600

    add extension get url for firefox

--- about/about.js
+++ about/about.js
@@ -1,3 +1,9 @@
-var imgURL = chrome.extension.getURL("icons/icon.svg");
+var bIsChrome = /Chrome/.test(navigator.userAgent);
+var imgUrl;
+if (bIsChrome) {
+  imgUrl = chrome.extension.getURL("icons/icon.svg");
+} else {
+  imgUrl = browser.extension.getURL("icons/icon.svg");
+}
 var icon = document.getElementById('icon');
-icon.src = `${imgURL}`;
+icon.src = imgUrl;
