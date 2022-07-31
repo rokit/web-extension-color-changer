7a8bd8fe8db10687ac7e940ab8c8b7a8db7d350d
Luke <goodvibrato@gmail.com>
2022-07-31 00:50:41 -0500

    add ColorChanger struct

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,32 @@ mod util;
 use wasm_bindgen::prelude::*;
 use web_sys::*;
 
+pub struct ColorChanger {
+    pub hosts: Vec<String>,
+}
+
+impl ColorChanger {
+    pub fn new() -> ColorChanger {
+        ColorChanger { hosts: Vec::new() }
+    }
+
+    pub fn should_change_colors(&self, hostname: &str) -> bool {
+        let hostname = hostname.to_owned();
+        if self.hosts.contains(&hostname) {
+            log!("change colors");
+            return true;
+        }
+        log!("do not change colors");
+        false
+    }
+}
+
 #[wasm_bindgen(start)]
 pub async fn main() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+
+    let mut color_changer = ColorChanger::new();
+
     let window = web_sys::window().expect("Could not get the window.");
     let storage = window
         .local_storage()
@@ -32,12 +55,11 @@ pub async fn main() {
         None => vec![],
     };
 
-    if hosts.contains(&hostname) {
-        log!("hosts contained hostname");
-    }
+    color_changer.hosts = hosts;
 
+    color_changer.should_change_colors(&hostname);
     log!("hostname: {:?}", hostname);
-    log!("hosts: {:?}", hosts);
+    log!("hosts: {:?}", color_changer.hosts);
     log!("hello");
 }
 
