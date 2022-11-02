8d5c685b4d9d265c28315266205f71709886d2c1
Luke <goodvibrato@gmail.com>
2022-11-02 06:27:44 -0500

    i

---
+++ interfaces.ts
@@ -0,0 +1,28 @@
+export interface State {
+  changeColors: boolean;
+  always: boolean;
+  activeTabId: number | null;
+  activeTabHostname: string;
+  fg: Color;
+  bg: Color;
+  li: Color;
+  activeBtn: string;
+  lightness: number;
+  hosts: string[];
+}
+
+export interface Swatch {
+  hue: number;
+  saturation: number;
+  lightness: number;
+  chosenId: string;
+}
+
+export interface Color {
+  swatch: Swatch,
+  hsl: string;
+  lightnessShift: string;
+  hueHovered: string;
+  hueVisited: string;
+  alpha: string;
+}
