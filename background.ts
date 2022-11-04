7af5d451ef3fa1640214908ac84110480369e82a
Luke <goodvibrato@gmail.com>
2022-11-03 20:46:34 -0500

    reorganize

--- background.ts
+++ background.ts
@@ -1,16 +1,39 @@
 
 import { ALWAYS, BACK_BTN, CHANGE_COLORS, CHANGE_LIGHTNESS, DEFAULT_STATE, FORE_BTN, GET_STATE, LINK_BTN, RESET, SET_ACTIVE_BUTTON, UPDATE_CHOSEN_COLOR, UPDATE_CONTENT } from "./constants";
-import { CanvasSwatch, Color, Message, State, Swatch } from "./interfaces";
+import { CanvasSwatch, Color, Message, State } from "./interfaces";
 import { setHslStrings } from "./utils";
 
 let state: State = JSON.parse(JSON.stringify(DEFAULT_STATE));
 
-function updateColor(color: Color, swatch: CanvasSwatch) {
-  color.swatch.hue = swatch.hue;
-  color.swatch.saturation = swatch.saturation;
-  color.swatch.lightness = swatch.lightness;
-  color.swatch.chosenId = swatch.id;
-  setHslStrings(color);
+// --------------------------------------------------------------------------------------------- actions
+function onMessage(req: Message, _sender: any, res: any): boolean {
+  switch (req.message) {
+    case GET_STATE: {
+      res(state);
+    }; break;
+    case CHANGE_COLORS: {
+      onChangeColors(req.payload);
+    }; break;
+    case ALWAYS: {
+      onAlways(req.payload);
+    }; break;
+    case SET_ACTIVE_BUTTON: {
+      onSetActiveButton(req.payload);
+    }; break;
+    case UPDATE_CHOSEN_COLOR: {
+      onUpdateChosenColor(req.payload);
+    }; break;
+    case CHANGE_LIGHTNESS: {
+      onChangeLightness(req.payload);
+    }; break;
+    case RESET: {
+      onReset();
+    }; break;
+    default: break;
+  }
+
+  // Allows the caller to await a response.
+  return true;
 }
 
 function onChangeColors(changeColors: boolean) {
@@ -35,49 +58,88 @@ function onAlways(always: boolean) {
   sendTabMessage({ message: UPDATE_CONTENT, payload: state });
 }
 
-function onContextMenuClicked(info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) {
-  let isChecked = info.checked ? info.checked : false;
-  if (info.menuItemId === CHANGE_COLORS) {
-    onChangeColors(isChecked)
-  } else if (info.menuItemId === ALWAYS) {
-    onAlways(isChecked)
+function onSetActiveButton(button: string) {
+  state.activeBtn = button;
+
+  if (button == FORE_BTN) {
+    state.lightness = state.fg.swatch.lightness;
+  } else if (button == BACK_BTN) {
+    state.lightness = state.bg.swatch.lightness;
+  } else if (button == LINK_BTN) {
+    state.lightness = state.li.swatch.lightness;
   }
-  chrome.contextMenus.update(CHANGE_COLORS, { checked: state.changeColors });
-  chrome.contextMenus.update(ALWAYS, { checked: state.always });
+
+  saveStorageAsync(state);
 }
 
-function createContextMenu() {
-  let ctxColorChanger: object = {
-    id: CHANGE_COLORS,
-    title: "Change Colors",
-    contexts: ["all"],
-    type: "checkbox",
-    checked: state.changeColors,
-  };
-
-  let ctxAlways: object = {
-    id: ALWAYS,
-    title: "Always",
-    contexts: ["all"],
-    type: "checkbox",
-    checked: state.always,
-  };
-
-  chrome.contextMenus.removeAll();
-  chrome.contextMenus.create(ctxColorChanger);
-  chrome.contextMenus.create(ctxAlways);
-  chrome.contextMenus.onClicked.removeListener(onContextMenuClicked);
-  chrome.contextMenus.onClicked.addListener(onContextMenuClicked);
+function onUpdateChosenColor(swatch: CanvasSwatch) {
+  switch (state.activeBtn) {
+    case FORE_BTN: {
+      updateColor(state.fg, swatch);
+    } break;
+    case BACK_BTN: {
+      updateColor(state.bg, swatch);
+    } break;
+    case LINK_BTN: {
+      updateColor(state.li, swatch);
+    } break;
+    default: break;
+  }
+  saveStorageAsync(state);
+  sendTabMessage({ message: UPDATE_CONTENT, payload: state });
 }
 
-// function updateContextMenu(changeColors, always) {
-//   if (!contextMenuCreated) return;
-//   chrome.contextMenus.update('changeColors', { checked: changeColors });
-//   chrome.contextMenus.update('always', { checked: always });
-// }
+function updateColor(color: Color, swatch: CanvasSwatch) {
+  color.swatch.hue = swatch.hue;
+  color.swatch.saturation = swatch.saturation;
+  color.swatch.lightness = swatch.lightness;
+  color.swatch.chosenId = swatch.id;
+  setHslStrings(color);
+}
 
+function onChangeLightness(lightness: number) {
+  state.lightness = lightness;
 
+  switch (state.activeBtn) {
+    case FORE_BTN: {
+      state.fg.swatch.lightness = lightness;
+      setHslStrings(state.fg);
+    } break;
+    case BACK_BTN: {
+      state.bg.swatch.lightness = lightness;
+      setHslStrings(state.bg);
+    } break;
+    case LINK_BTN: {
+      state.li.swatch.lightness = lightness;
+      setHslStrings(state.li);
+    } break;
+    default: break;
+  }
 
+  saveStorageAsync(state);
+  sendTabMessage({ message: UPDATE_CONTENT, payload: state });
+}
+
+/** Sets back to defaults. Does not reset:
+ * changeColors
+ * activeTabId
+ * activeTabHostname
+*/
+function onReset() {
+  let defaultState = JSON.parse(JSON.stringify(DEFAULT_STATE));
+  state.always = defaultState.always;
+  state.hosts = defaultState.hosts;
+  state.fg = defaultState.fg;
+  state.bg = defaultState.bg;
+  state.li = defaultState.li;
+  state.activeBtn = defaultState.activeBtn;
+  state.lightness = defaultState.lightness;
+
+  saveStorageAsync(state);
+  sendTabMessage({ message: UPDATE_CONTENT, payload: state });
+}
+
+// --------------------------------------------------------------------------------------------- tabs
 /** On tab activation, get the full tab data. */
 function onTabActivated(tabInfo: chrome.tabs.TabActiveInfo) {
   state.activeTabId = tabInfo.tabId;
@@ -111,79 +173,6 @@ function validateTab(tab: chrome.tabs.Tab) {
   sendTabMessage({ message: UPDATE_CONTENT, payload: state });
 }
 
-function onUpdateChosenColor(swatch: CanvasSwatch) {
-  switch (state.activeBtn) {
-    case FORE_BTN: {
-      updateColor(state.fg, swatch);
-    } break;
-    case BACK_BTN: {
-      updateColor(state.bg, swatch);
-    } break;
-    case LINK_BTN: {
-      updateColor(state.li, swatch);
-    } break;
-    default: break;
-  }
-  saveStorageAsync(state);
-  sendTabMessage({ message: UPDATE_CONTENT, payload: state });
-}
-
-function onSetActiveButton(button: string) {
-  state.activeBtn = button;
-
-  if (button == FORE_BTN) {
-    state.lightness = state.fg.swatch.lightness;
-  } else if (button == BACK_BTN) {
-    state.lightness = state.bg.swatch.lightness;
-  } else if (button == LINK_BTN) {
-    state.lightness = state.li.swatch.lightness;
-  }
-
-  saveStorageAsync(state);
-}
-
-/** Sets back to defaults. Does not reset:
- * changeColors
- * activeTabId
- * activeTabHostname
-*/
-function onReset() {
-  let defaultState = JSON.parse(JSON.stringify(DEFAULT_STATE));
-  state.always = defaultState.always;
-  state.hosts = defaultState.hosts;
-  state.fg = defaultState.fg;
-  state.bg = defaultState.bg;
-  state.li = defaultState.li;
-  state.activeBtn = defaultState.activeBtn;
-  state.lightness = defaultState.lightness;
-
-  saveStorageAsync(state);
-  sendTabMessage({ message: UPDATE_CONTENT, payload: state });
-}
-
-function onChangeLightness(lightness: number) {
-  state.lightness = lightness;
-
-  switch (state.activeBtn) {
-    case FORE_BTN: {
-      state.fg.swatch.lightness = lightness;
-      setHslStrings(state.fg);
-    } break;
-    case BACK_BTN: {
-      state.bg.swatch.lightness = lightness;
-      setHslStrings(state.bg);
-    } break;
-    case LINK_BTN: {
-      state.li.swatch.lightness = lightness;
-      setHslStrings(state.li);
-    } break;
-    default: break;
-  }
-
-  saveStorageAsync(state);
-  sendTabMessage({ message: UPDATE_CONTENT, payload: state });
-}
-
 //** Send message to a tab. If the extension was reloaded, the tab will not be able to receive any messages until reloaded, hence the catch block. */
 async function sendTabMessage(message: Message) {
   if (!state.activeTabId) return;
@@ -196,34 +185,40 @@ async function sendTabMessage(message: Message) {
   }
 }
 
-function onMessage(req: Message, _sender: any, res: any): boolean {
-  switch (req.message) {
-    case GET_STATE: {
-      res(state);
-    }; break;
-    case CHANGE_COLORS: {
-      onChangeColors(req.payload);
-    }; break;
-    case ALWAYS: {
-      onAlways(req.payload);
-    }; break;
-    case SET_ACTIVE_BUTTON: {
-      onSetActiveButton(req.payload);
-    }; break;
-    case UPDATE_CHOSEN_COLOR: {
-      onUpdateChosenColor(req.payload);
-    }; break;
-    case CHANGE_LIGHTNESS: {
-      onChangeLightness(req.payload);
-    }; break;
-    case RESET: {
-      onReset();
-    }; break;
-    default: break;
-  }
+// --------------------------------------------------------------------------------------------- context menu
+function createContextMenu() {
+  let ctxColorChanger: object = {
+    id: CHANGE_COLORS,
+    title: "Change Colors",
+    contexts: ["all"],
+    type: "checkbox",
+    checked: state.changeColors,
+  };
 
-  // Allows the caller to await a response.
-  return true;
+  let ctxAlways: object = {
+    id: ALWAYS,
+    title: "Always",
+    contexts: ["all"],
+    type: "checkbox",
+    checked: state.always,
+  };
+
+  chrome.contextMenus.removeAll();
+  chrome.contextMenus.create(ctxColorChanger);
+  chrome.contextMenus.create(ctxAlways);
+  chrome.contextMenus.onClicked.removeListener(onContextMenuClicked);
+  chrome.contextMenus.onClicked.addListener(onContextMenuClicked);
+}
+
+function onContextMenuClicked(info: chrome.contextMenus.OnClickData, _tab?: chrome.tabs.Tab) {
+  let isChecked = info.checked ? info.checked : false;
+  if (info.menuItemId === CHANGE_COLORS) {
+    onChangeColors(isChecked)
+  } else if (info.menuItemId === ALWAYS) {
+    onAlways(isChecked)
+  }
+  chrome.contextMenus.update(CHANGE_COLORS, { checked: state.changeColors });
+  chrome.contextMenus.update(ALWAYS, { checked: state.always });
 }
 
 // --------------------------------------------------------------------------------------------- installed
@@ -281,10 +276,9 @@ function clearStorage() {
 chrome.tabs.onActivated.addListener(onTabActivated);
 chrome.runtime.onMessage.addListener(onMessage);
 chrome.runtime.onInstalled.addListener(onInstalled);
-// chrome.storage.onChanged.addListener(onStorageChanged);
 
 // --------------------------------------------------------------------------------------------- init
-/** If state hasn't been previously set in storage, initialize it, otherwise overwrite the default state. */
+/** Get state from storage if it exists. If not, create default state. */
 async function initServiceWorker() {
   let storageState = await getStorageAsync();
   if (storageState) {
