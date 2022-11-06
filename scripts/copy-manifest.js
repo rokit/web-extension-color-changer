50fb5eb278291a423fb83aa1f60004bec06461e0
Luke <goodvibrato@gmail.com>
2022-11-06 16:53:51 -0600

    add script to autocopy manifest and build

---
+++ scripts/copy-manifest.js
@@ -0,0 +1,21 @@
+// require fs module
+const fs = require("fs");
+
+var args = process.argv.slice(2);
+if (args.length === 0) {
+  console.error("Specify manifest.ff.json or manifest.chrome.json.");
+  return;
+}
+
+const source = args[0];
+const manifestJson = "manifest.json";
+
+// copy contents from source file to destination file
+fs.copyFile(source, manifestJson, (error) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  console.log(`Copied contents of ${args[0]} to ${manifestJson}`);
+});
