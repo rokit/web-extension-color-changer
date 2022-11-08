bb050f817079ac033db6eaec3ae72194fe78b958
Luke <goodvibrato@gmail.com>
2022-11-07 20:58:45 -0600

    simplify css

--- content.ts
+++ content.ts
@@ -11,59 +11,49 @@ var observerConfig = { attributes: true, attributeFilter: ["class"] };
 var css = "";
 
 function updateCss(state: State) {
-  let not = ':not(img):not(img *):not(video):not(video *):not(svg):not(svg *):not(.rc-VideoMiniPlayer *)';
   if (!state) return;
 
+  let not = ':not(img, video, svg, .rc-VideoMiniPlayer)';
+
   css = `
-  .${COLOR_CHANGER_CLASS_NAME} *${not} {
-    color: ${state.fg.hsl} !important;
+  html.${COLOR_CHANGER_CLASS_NAME},
+  html.${COLOR_CHANGER_CLASS_NAME} body,
+  html.${COLOR_CHANGER_CLASS_NAME} * ${not}
+  {
+    color: ${state.fg.hsl};
     background-color: ${state.bg.hsl} !important;
     border-color: ${state.bg.lightnessShift} !important;
   }
-  .${COLOR_CHANGER_CLASS_NAME}${not} *:before,
-  .${COLOR_CHANGER_CLASS_NAME}${not} *:after {
-    color: ${state.fg.hsl} !important;
-    background: ${state.bg.hsl} !important;
-    border-color: ${state.bg.lightnessShift} !important;
-  }
-  .${COLOR_CHANGER_CLASS_NAME}${not} img {
-    visibility: visible !important;
-  }
-  .${COLOR_CHANGER_CLASS_NAME}${not} button {
-    color: ${state.li.hsl} !important;
-  }
 
-  .${COLOR_CHANGER_CLASS_NAME}${not} input,
-  .${COLOR_CHANGER_CLASS_NAME}${not} input *,
-  .${COLOR_CHANGER_CLASS_NAME}${not} textarea,
-  .${COLOR_CHANGER_CLASS_NAME}${not} textarea *,
-  .${COLOR_CHANGER_CLASS_NAME}${not} pre,
-  .${COLOR_CHANGER_CLASS_NAME}${not} pre *,
-  .${COLOR_CHANGER_CLASS_NAME}${not} code,
-  .${COLOR_CHANGER_CLASS_NAME}${not} code *
-  {
-    background-color: ${state.bg.lightnessShift} !important;
-  }
-
-  .${COLOR_CHANGER_CLASS_NAME}${not} a,
-  .${COLOR_CHANGER_CLASS_NAME}${not} a *
+  html.${COLOR_CHANGER_CLASS_NAME} button,
+  html.${COLOR_CHANGER_CLASS_NAME} button *
   {
     color: ${state.li.hsl} !important;
-    background-color: ${state.bg.hsl} !important;
+    background-color: transparent !important;
   }
-  .${COLOR_CHANGER_CLASS_NAME}${not} a:hover,
-  .${COLOR_CHANGER_CLASS_NAME}${not} a:hover *
+  html.${COLOR_CHANGER_CLASS_NAME} a,
+  html.${COLOR_CHANGER_CLASS_NAME} a *
+  {
+    color: ${state.li.hsl} !important;
+    background-color: transparent !important;
+  }
+  html.${COLOR_CHANGER_CLASS_NAME} a:hover,
+  html.${COLOR_CHANGER_CLASS_NAME} a:hover *
   {
     color: ${state.li.hueHovered} !important;
+    background-color: transparent !important;
   }
-  .${COLOR_CHANGER_CLASS_NAME}${not} a:active,
-  .${COLOR_CHANGER_CLASS_NAME}${not} a:active *
+  html.${COLOR_CHANGER_CLASS_NAME} a:active,
+  html.${COLOR_CHANGER_CLASS_NAME} a:active *
   {
     color: ${state.li.hueVisited} !important;
+    background-color: transparent !important;
   }
-  .${COLOR_CHANGER_CLASS_NAME}${not} a:visited,
-  .${COLOR_CHANGER_CLASS_NAME}${not} a:visited * {
+  html.${COLOR_CHANGER_CLASS_NAME} a:visited,
+  html.${COLOR_CHANGER_CLASS_NAME} a:visited *
+  {
     color: ${state.li.hueVisited} !important;
+    background-color: transparent !important;
   }
 `;
 }
