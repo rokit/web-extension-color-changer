d291a732626226b2842634e4e5a92adda2813b1a
Luke Simpson <lsimpsonv1@gmail.com>
2026-07-19 19:43:22 -0500

    c

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,6 @@
 import convert from "color-convert";
 import { type Color, type Message, type State } from "./interfaces";
 
-// export const isChrome = /Chrome/.test(navigator.userAgent);
-
 const DEG_TO_RAD = Math.PI / 180;
 const RAD_TO_DEG = 180 / Math.PI;
 
@@ -69,38 +67,3 @@ export function shouldChangeColors(state: State): boolean {
 export function roundToTenths(num: number) {
   return Math.round((num + Number.EPSILON) * 10) / 10;
 }
-
-// /** Workaround for not being able to await an async message with chrome.runtime in Firefox.
-//  * https://bugzilla.mozilla.org/show_bug.cgi?id=1228044
-// */
-// export function runtimeSendMessage(message: Message): Promise<any> {
-//   if (isChrome) {
-//     return chrome.runtime.sendMessage(message);
-//   } else {
-//     return browser.runtime.sendMessage(message);
-//   }
-// }
-
-// export function saveState(state: State) {
-//   if (isChrome) {
-//     chrome.storage.sync.set({ 'colorChangerState': state });
-//   } else {
-//     browser.storage.sync.set({ 'colorChangerState': state });
-//   }
-// }
-
-// export function getState() {
-//   if (isChrome) {
-//     return chrome.storage.sync.get(['colorChangerState']);
-//   } else {
-//     return browser.storage.sync.get(['colorChangerState']);
-//   }
-// }
-
-// export function tabsQuery(query: any) {
-//   if (isChrome) {
-//     return chrome.tabs.query(query);
-//   } else {
-//     return browser.tabs.query(query) as Promise<chrome.tabs.Tab[]>;
-//   }
-// }
