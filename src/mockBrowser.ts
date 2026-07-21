9951cc1e5986d481040f68ccbbc70e1be5d4c8b1
Luke Simpson <lsimpsonv1@gmail.com>
2026-07-21 10:52:45 -0500

    fix mock browser

--- src/mockBrowser.ts
+++ src/mockBrowser.ts
@@ -1,27 +1,46 @@
 import * as c from "./constants";
-import type { Color, State } from "./types";
+import type { Color, Message, State } from "./types";
 import { setHslStrings } from "./utils";
 
 export class MockBrowser {
     state: State
     storage: object
     runtime: object
+    tabs: object
+    contextMenus: object
 
     constructor() {
         this.state = JSON.parse(JSON.stringify(c.DEFAULT_STATE)) as State;
         this.state.activeTabId = 1;
 
         this.storage = {
+            sync: {
+                get: (keys: string[]) => {
+                    return Object.fromEntries(keys.map(key => [key, this.state[key as keyof State]]));;
+                },
+                set: (items: { [key: string]: any; }) => {
+                    this.state = { ...this.state, ...items }
+                }
+            },
             onChanged: {
                 addListener: (listener: any) => {
                     setInterval(() => listener(), 1000 / 60)
                 }
-            }
+            },
         }
 
         this.runtime = {
             sendMessage: (message: any) => {
             }
+        }
+
+        this.tabs = {
+            sendMessage: (message: Message) => {
+            }
+        }
+
+        this.contextMenus = {
+            update: (id: string | number, updateProperties: browser.contextMenus._UpdateUpdateProperties) => { }
         }
     }
 }
